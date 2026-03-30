6e6d1ecd9e9e15a6723223cd793727d16d689ca8
JulioSchwartz <j.ulioschwartz@hotmail.com>
2026-03-29 21:13:54 -0300

    Update page.tsx

--- src/app/orcamentos/editar/[id]/page.tsx
+++ src/app/orcamentos/editar/[id]/page.tsx
@@ -283,43 +283,60 @@ export default function EditarOrcamento() {
           </div>
 
           {itens.filter(i => i.categoria === cat).map((item, index) => (
-           <div key={index} style={linha(index)}>
-  <input value={item.codigo} onChange={e => atualizarItem(index,'codigo',e.target.value)} style={inputPeq}/>
-  <input value={item.descricao} onChange={e => atualizarItem(index,'descricao',e.target.value)} style={input}/>
-  <input value={item.unidade} onChange={e=>atualizarItem(index,'unidade',e.target.value)} style={inputPeq}/>
+  <div key={index} style={linha(index)}>
 
-  <input
-    type="number"
-    value={item.quantidade}
-    onChange={e => atualizarItem(index,'quantidade', Number(e.target.value))}
-    style={inputPeq}
-  />
+    <input
+      value={item.codigo}
+      onChange={e => atualizarItem(index,'codigo',e.target.value)}
+      style={inputPeq}
+    />
 
-  <input
-    type="number"
-    value={item.material}
-    onChange={e => atualizarItem(index,'material', Number(e.target.value))}
-    style={inputPeq}
-  />
+    <input
+      value={item.descricao}
+      onChange={e => atualizarItem(index,'descricao',e.target.value)}
+      style={input}
+    />
 
-  <input
-    type="number"
-    value={item.mao_obra}
-    onChange={e => atualizarItem(index,'mao_obra', Number(e.target.value))}
-    style={inputPeq}
-  />
+    <input
+      value={item.unidade}
+      onChange={e=>atualizarItem(index,'unidade',e.target.value)}
+      style={inputPeq}
+    />
 
-  <input
-    type="number"
-    value={item.equipamentos}
-    onChange={e => atualizarItem(index,'equipamentos', Number(e.target.value))}
-    style={inputPeq}
-  />
+    <input
+      type="number"
+      value={item.quantidade}
+      onChange={e => atualizarItem(index,'quantidade', Number(e.target.value))}
+      style={inputPeq}
+    />
 
-  <strong>R$ {totalItem(item).toFixed(2)}</strong>
+    <input
+      type="number"
+      value={item.material}
+      onChange={e => atualizarItem(index,'material', Number(e.target.value))}
+      style={inputPeq}
+    />
 
-  <button onClick={()=>removerItem(index)} style={btnRemover}>X</button>
-</div>
+    <input
+      type="number"
+      value={item.mao_obra}
+      onChange={e => atualizarItem(index,'mao_obra', Number(e.target.value))}
+      style={inputPeq}
+    />
+
+    <input
+      type="number"
+      value={item.equipamentos}
+      onChange={e => atualizarItem(index,'equipamentos', Number(e.target.value))}
+      style={inputPeq}
+    />
+
+    <strong>R$ {totalItem(item).toFixed(2)}</strong>
+
+    <button onClick={()=>removerItem(index)} style={btnRemover}>X</button>
+
+  </div>
+))}
 
               <strong>R$ {totalItem(item).toFixed(2)}</strong>
               <button onClick={()=>removerItem(index)} style={btnRemover}>X</button>
