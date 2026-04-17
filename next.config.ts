f7c6f06d14fe919064f1483f5f6346e26e1ca7f9
JulioSchwartz <j.ulioschwartz@hotmail.com>
2026-04-17 15:13:43 -0300

    fix: service worker reconfigurado

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ const withPWAConfig = withPWA({
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
   disable: process.env.NODE_ENV === 'development',
-  customWorkerSrc: 'sw-custom.js',
   workboxOptions: {
     disableDevLogs: true,
   },
